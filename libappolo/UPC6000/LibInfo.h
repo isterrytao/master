68fea990732b6039f1efdb345b7213e6279daf06
UD00004 <dlw@udantech.com>
2018-01-15 13:33:43 +0800

    更新模板库到最新 539b7e34d99e30cfe63077ddfff84f98a8413c75

--- libappolo/UPC6000/LibInfo.h
+++ libappolo/UPC6000/LibInfo.h
@@ -32,17 +32,17 @@
 /**
  * 以下宏定义由cmake自动生成,不需要手动修改
  */
-#define LIB_INFO_SOURCE_COMMIT_ID        "539b7e34d99e30cfe63077ddfff84f98a8413c75"
-#define LIB_INFO_CONFIG_COMMIT_ID        "539b7e34d99e30cfe63077ddfff84f98a8413c75"
-#define LIB_INFO_BUILD_TIME              "20180115102343"
+#define LIB_INFO_SOURCE_COMMIT_ID        "539b7e34d99e30cfe630"
+#define LIB_INFO_CONFIG_COMMIT_ID        "539b7e34d99e30cfe630"
+#define LIB_INFO_BUILD_TIME              "20180115133146"
 
 #define LIB_INFO_BUILD_DATE_Y            2018
 #define LIB_INFO_BUILD_DATE_M            1
 #define LIB_INFO_BUILD_DATE_D            15
 
-#define LIB_INFO_BUILD_TIME_H            10
-#define LIB_INFO_BUILD_TIME_M            23
-#define LIB_INFO_BUILD_TIME_S            43
+#define LIB_INFO_BUILD_TIME_H            13
+#define LIB_INFO_BUILD_TIME_M            31
+#define LIB_INFO_BUILD_TIME_S            46
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
