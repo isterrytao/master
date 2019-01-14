4c8ab8806378654fb5ac03ccad3a844216fdf72e
UD00004 <dlw@udantech.com>
2019-01-14 14:11:02 +0800

    更新模板库到最新 f065c2b4f532824687bcc0a144688926e58d5b9f

--- libappolo/UPA650/LibInfo.h
+++ libappolo/UPA650/LibInfo.h
@@ -14,21 +14,21 @@
 #define FWVERSION_MAJOR                   3
 #define FWVERSION_MINOR                   0
 #define FWVERSION_REVISION                3
-#define FWVERSION_BUILD                   7
-#define LIB_INFO_FWVERSION                "3.0.3.17"
+#define FWVERSION_BUILD                   8
+#define LIB_INFO_FWVERSION                "3.0.3.18"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "de66ffe7e8ac704e294cc60eadec9b73dd5de3b2"
-#define LIB_INFO_CONFIG_COMMIT_ID        "de66ffe7e8ac704e294cc60eadec9b73dd5de3b2"
-#define LIB_INFO_BUILD_TIME              "20190114094945"
+#define LIB_INFO_SOURCE_COMMIT_ID        "f065c2b4f532824687bcc0a144688926e58d5b9f"
+#define LIB_INFO_CONFIG_COMMIT_ID        "f065c2b4f532824687bcc0a144688926e58d5b9f"
+#define LIB_INFO_BUILD_TIME              "20190114140659"
 
 #define LIB_INFO_BUILD_DATE_Y            2019
 #define LIB_INFO_BUILD_DATE_M            1
 #define LIB_INFO_BUILD_DATE_D            14
 
-#define LIB_INFO_BUILD_TIME_H            9
-#define LIB_INFO_BUILD_TIME_M            49
-#define LIB_INFO_BUILD_TIME_S            45
+#define LIB_INFO_BUILD_TIME_H            14
+#define LIB_INFO_BUILD_TIME_M            6
+#define LIB_INFO_BUILD_TIME_S            59
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
--- libconfig.js
+++ libconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     LibInfo: {
-	    masterVersion: "3.0.3.17",
+	    masterVersion: "3.0.3.18",
         releaseName: "AppoloLib",
     },
 
