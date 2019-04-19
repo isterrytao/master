ac5a174d8b029516cb1e5f5d78c7a8a2cb30f216
UD00004 <dlw@udantech.com>
2019-04-19 15:26:12 +0800

    更新模板库到最新 3e0228faf59afa5ab6d7cb3cb148b522dccfc8c7

--- libappolo/UPA640/LibInfo.h
+++ libappolo/UPA640/LibInfo.h
@@ -14,21 +14,21 @@
 #define FWVERSION_MAJOR                   3
 #define FWVERSION_MINOR                   0
 #define FWVERSION_REVISION                3
-#define FWVERSION_BUILD                   5
-#define LIB_INFO_FWVERSION                "3.0.3.25"
+#define FWVERSION_BUILD                   6
+#define LIB_INFO_FWVERSION                "3.0.3.26"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "596322a95d8cc9ec074cea6f02dc4728de03ed3f"
-#define LIB_INFO_CONFIG_COMMIT_ID        "596322a95d8cc9ec074cea6f02dc4728de03ed3f"
-#define LIB_INFO_BUILD_TIME              "20190415160203"
+#define LIB_INFO_SOURCE_COMMIT_ID        "3e0228faf59afa5ab6d7cb3cb148b522dccfc8c7"
+#define LIB_INFO_CONFIG_COMMIT_ID        "3e0228faf59afa5ab6d7cb3cb148b522dccfc8c7"
+#define LIB_INFO_BUILD_TIME              "20190419152247"
 
 #define LIB_INFO_BUILD_DATE_Y            2019
 #define LIB_INFO_BUILD_DATE_M            4
-#define LIB_INFO_BUILD_DATE_D            15
+#define LIB_INFO_BUILD_DATE_D            19
 
-#define LIB_INFO_BUILD_TIME_H            16
-#define LIB_INFO_BUILD_TIME_M            2
-#define LIB_INFO_BUILD_TIME_S            3
+#define LIB_INFO_BUILD_TIME_H            15
+#define LIB_INFO_BUILD_TIME_M            22
+#define LIB_INFO_BUILD_TIME_S            47
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
