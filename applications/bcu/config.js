06ceb3081702fa09f94d986f2557260f207bfa5a
UD00004 <dlw@udantech.com>
2019-08-01 15:48:36 +0800

    更新模板库到最新 9d661b83b65b0849d9bf23f35e5fc513305e33b9

--- applications/bcu/config.js
+++ applications/bcu/config.js
@@ -183,7 +183,7 @@ module.exports = {
             // 通道1
             notes:[
                 {section:"model", chs:"传感器型号", comments:'可选: "DHAB_S124_1"(DHAB_S124大量程), "DHAB_S124_2"(DHAB_S124小量程), DHAB_S118_1"(DHAB_S118大量程), "DHAB_S118_2"(DHAB_S118小量程), "FS300E2T"(飞轩FS300E2T,300A量程), "FS500E2T"(飞轩FS500E2T,500A量程), "FS600E2T"(飞轩FS600E2T,600A量程), "FS700E2T"(飞轩FS700E2T,700A量程), "WHK30_350DHAB5S2L_1"(启东双赢30/350A大量程), "WHK30_350DHAB5S2L_2"(启东双赢30/350A小量程), "EHMWX911C500"(松下EHMWX911C500,500A量程), "HAH1BVW_S02"(LEM 200A单量程), "HAH1BVW_S03"(LEM 300A单量程), "HAH1BV_S02"(LEM 500A单量程),'
-                                                                    +'"WHK20BS5S2"(启东双赢 20A单量程), "WHK50BS5S2"(启东双赢 50A单量程), "FS300BT"(飞轩300A单量程), "FS600BT"(飞轩600A单量程), "WHK300EA5S2S"(启东双赢300A单量程), "WHK500EA5S2S"(启东双赢500A单量程)'},
+                                                                    +'"WHK20BS5S2"(启东双赢 20A单量程), "WHK50BS5S2"(启东双赢 50A单量程), "WHK100EA5S2S"(启东双赢 100A单量程), "WHK200EA5S2S"(启东双赢 200A单量程), "WHK300EA5S2S"(启东双赢300A单量程), "WHK400EA5S2S"(启东双赢400A单量程), "WHK500EA5S2S"(启东双赢500A单量程), "WHK600EA5S2S"(启东双赢600A单量程), "FS300BT"(飞轩300A单量程), "FS600BT"(飞轩600A单量程) '},
                 {section:"filterNum", chs:"滤波的采集次数"},
                 {section:"dropNum", chs:"极值丢弃的个数"},
             ],
