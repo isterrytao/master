5f7fa05c48df0208a188096ae7f698f4903c2c83
yuanyuan.li <yuanyuan.li@udantech.com>
2020-08-04 14:35:34 +0800

    增加施能通信协议

--- applications/bcu/config.js
+++ applications/bcu/config.js
@@ -258,7 +258,7 @@ module.exports = {
 
     CommProtocol: {
         notes: [
-            {section:"chg", chs: "充电机通讯协议类型", comments: '"Default", "UDAN", "DongYaoV2.0", "ShiNeng", "UDAN_KaiYuan_DongYaoV1.0", "UDAN_ShiNeng","YiJiaTongV2.0", "QianGaiLi", "BanYiTong_Chg_V1.2", "ADY_V1.1", "CAN2.0C", "TieCheng_Soc", "LongGong_Chg"'},
+            {section:"chg", chs: "充电机通讯协议类型", comments: '"Default", "UDAN", "DongYaoV2.0", "ShiNeng", "UDAN_KaiYuan_DongYaoV1.0", "UDAN_ShiNeng","YiJiaTongV2.0", "QianGaiLi", "BanYiTong_Chg_V1.2", "ADY_V1.1", "CAN2.0C", "TieCheng_Soc", "LongGong_Chg", "ShiNeng_Heat"'},
             {section:"vcu", chs: "VCU通讯协议类型", comments: '"Default", "UDAN", "QianTuo_CAN20A_125K", "QianTuo_CAN20B_250K", "QianTuo_CAN20A_125K_V1.02", "CHOLIFT", "CAN_1232E_201809", "ZOWELL_V2.0", "XianFeng_V3.0", "BaoLi", "BanYiTong_Vcu_V1.2", "RuYi_Vcu", "CAN2.0F", "BaoLi_20200622", "Vcu_250k", "BMS_CAN_3.0", "LongGong_Vcu"'},
         ],
         chg: "UDAN",
