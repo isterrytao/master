86996223ac417584d479f6bfafecc7af60963dd9
yuanyuan.li <yuanyuan.li@udantech.com>
2020-09-09 18:33:42 +0800

    增加屏供电控制及上报

--- applications/bcu/UserStrategy.c
+++ applications/bcu/UserStrategy.c
@@ -133,7 +133,18 @@ static const UserStrategy_BuzzerAlarmType alarmLevel2Cfg[] = {
 
 void UserStrategy_Init(Async_LooperType *looper)
 {
+#ifdef RELAYM_FN_HMI
+    RuntimeM_RunModeType mode = RuntimeM_GetMode();
+#endif
     UserStrategy_innerData.currentIsAllowToPowerOff = TRUE;
+
+#ifdef RELAYM_FN_HMI
+    if(mode == RUNTIMEM_RUNMODE_NORMAL || mode == RUNTIMEM_RUNMODE_DATA || mode == RUNTIMEM_RUNMODE_CALIBRATE){
+        HLSS_Drive(RelayMConfigData[RELAYM_FN_HMI].DriveHSS, HLSS_DRIVE_ON);
+    }
+    (void)RelayM_Control(RELAYM_FN_HMI, RELAYM_CONTROL_ON);
+#endif
+
     if (looper != NULL)
     {
         UserCurrentSensor_Init(looper);
