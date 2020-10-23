1e05de28f176a06b7b550fca54dfde755029555d
UD00004 <dlw@udantech.com>
2020-10-23 13:29:32 +0800

    更新模板库到最新 2fee0be919cbddf502b12529743172b5e3758df9

--- libappolo/UPA640/LibInfo.h
+++ libappolo/UPA640/LibInfo.h
@@ -18,16 +18,16 @@
 #define LIB_INFO_FWVERSION                "3.0.5.1"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "f9c1971fb2ba4f12eec8a596748c8ffa8f363811"
-#define LIB_INFO_CONFIG_COMMIT_ID        "f9c1971fb2ba4f12eec8a596748c8ffa8f363811"
-#define LIB_INFO_BUILD_TIME              "20201010165408"
+#define LIB_INFO_SOURCE_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
+#define LIB_INFO_CONFIG_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
+#define LIB_INFO_BUILD_TIME              "20201023132608"
 
 #define LIB_INFO_BUILD_DATE_Y            2020
 #define LIB_INFO_BUILD_DATE_M            10
-#define LIB_INFO_BUILD_DATE_D            10
+#define LIB_INFO_BUILD_DATE_D            23
 
-#define LIB_INFO_BUILD_TIME_H            16
-#define LIB_INFO_BUILD_TIME_M            54
+#define LIB_INFO_BUILD_TIME_H            13
+#define LIB_INFO_BUILD_TIME_M            26
 #define LIB_INFO_BUILD_TIME_S            8
 
 #pragma push
--- libappolo/UPA650/LibInfo.h
+++ libappolo/UPA650/LibInfo.h
@@ -18,17 +18,17 @@
 #define LIB_INFO_FWVERSION                "3.0.5.1"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "f9c1971fb2ba4f12eec8a596748c8ffa8f363811"
-#define LIB_INFO_CONFIG_COMMIT_ID        "f9c1971fb2ba4f12eec8a596748c8ffa8f363811"
-#define LIB_INFO_BUILD_TIME              "20201010165259"
+#define LIB_INFO_SOURCE_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
+#define LIB_INFO_CONFIG_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
+#define LIB_INFO_BUILD_TIME              "20201023132517"
 
 #define LIB_INFO_BUILD_DATE_Y            2020
 #define LIB_INFO_BUILD_DATE_M            10
-#define LIB_INFO_BUILD_DATE_D            10
+#define LIB_INFO_BUILD_DATE_D            23
 
-#define LIB_INFO_BUILD_TIME_H            16
-#define LIB_INFO_BUILD_TIME_M            52
-#define LIB_INFO_BUILD_TIME_S            59
+#define LIB_INFO_BUILD_TIME_H            13
+#define LIB_INFO_BUILD_TIME_M            25
+#define LIB_INFO_BUILD_TIME_S            17
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
