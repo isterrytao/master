3eee8fefd0dbf9c866cb0efb235bbf8f002268cd
UD00004 <dlw@udantech.com>
2020-11-09 19:30:49 +0800

    更新模板库到最新 efb6338ea54efded4b3516933016c6f0542ee5ce

--- libappolo/UPA650/LibInfo.h
+++ libappolo/UPA650/LibInfo.h
@@ -18,17 +18,17 @@
 #define LIB_INFO_FWVERSION                "3.0.5.1"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
-#define LIB_INFO_CONFIG_COMMIT_ID        "2fee0be919cbddf502b12529743172b5e3758df9"
-#define LIB_INFO_BUILD_TIME              "20201023132517"
+#define LIB_INFO_SOURCE_COMMIT_ID        "efb6338ea54efded4b3516933016c6f0542ee5ce"
+#define LIB_INFO_CONFIG_COMMIT_ID        "efb6338ea54efded4b3516933016c6f0542ee5ce"
+#define LIB_INFO_BUILD_TIME              "20201109192622"
 
 #define LIB_INFO_BUILD_DATE_Y            2020
-#define LIB_INFO_BUILD_DATE_M            10
-#define LIB_INFO_BUILD_DATE_D            23
+#define LIB_INFO_BUILD_DATE_M            11
+#define LIB_INFO_BUILD_DATE_D            9
 
-#define LIB_INFO_BUILD_TIME_H            13
-#define LIB_INFO_BUILD_TIME_M            25
-#define LIB_INFO_BUILD_TIME_S            17
+#define LIB_INFO_BUILD_TIME_H            19
+#define LIB_INFO_BUILD_TIME_M            26
+#define LIB_INFO_BUILD_TIME_S            22
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
