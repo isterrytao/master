90f67a109b8b48b8ef959ddad4f6d06e43e12870
UD09025 <wei.wei@udantech.com>
2021-03-24 11:39:04 +0800

    更新模板库到最新 097f43a0a07d16a656bd409afc789552233b95ac

--- libappolo/UPA650/LibInfo.h
+++ libappolo/UPA650/LibInfo.h
@@ -11,24 +11,24 @@
 
 #include "BootInfo.h"
 
-#define FWVERSION_MAJOR                   3
+#define FWVERSION_MAJOR                   4
 #define FWVERSION_MINOR                   0
-#define FWVERSION_REVISION                5
-#define FWVERSION_BUILD                   1
-#define LIB_INFO_FWVERSION                "3.0.5.1"
+#define FWVERSION_REVISION                0
+#define FWVERSION_BUILD                   0
+#define LIB_INFO_FWVERSION                "4.0.0.0"
 
 
-#define LIB_INFO_SOURCE_COMMIT_ID        "1a2fea9c118731f5cff43bc8d369b2f18c9e285a"
-#define LIB_INFO_CONFIG_COMMIT_ID        "1a2fea9c118731f5cff43bc8d369b2f18c9e285a"
-#define LIB_INFO_BUILD_TIME              "20210313164007"
+#define LIB_INFO_SOURCE_COMMIT_ID        "097f43a0a07d16a656bd409afc789552233b95ac"
+#define LIB_INFO_CONFIG_COMMIT_ID        "097f43a0a07d16a656bd409afc789552233b95ac"
+#define LIB_INFO_BUILD_TIME              "20210324113541"
 
 #define LIB_INFO_BUILD_DATE_Y            2021
 #define LIB_INFO_BUILD_DATE_M            3
-#define LIB_INFO_BUILD_DATE_D            13
+#define LIB_INFO_BUILD_DATE_D            24
 
-#define LIB_INFO_BUILD_TIME_H            16
-#define LIB_INFO_BUILD_TIME_M            40
-#define LIB_INFO_BUILD_TIME_S            7
+#define LIB_INFO_BUILD_TIME_H            11
+#define LIB_INFO_BUILD_TIME_M            35
+#define LIB_INFO_BUILD_TIME_S            41
 
 #pragma push
 #pragma CONST_SEG __GPAGE_SEG APP_INFO
